f04cd30a899b78c8371b25b9915da748dfd70437
NavodContrath <navod.contrath@gmail.com>
2025-02-26 16:11:44 +0100

    add bonus for each and loop to fifth snack

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -2,6 +2,7 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+//filter
 const even = nums.filter((numb) => {
     if (numb % 2 == 0) {
         return true
@@ -9,3 +10,20 @@ const even = nums.filter((numb) => {
     return false
 })
 console.log(even)
+//for each
+const evenNumbers = []
+nums.forEach((numb) => {
+    if (numb % 2 === 0) {
+        evenNumbers.push(numb)
+    }
+})
+console.log(evenNumbers)
+//for loop
+const evenNUmbers = []
+for (let i = 0; i < nums.length; i++) {
+    const thisNumb = nums[i]
+    if (thisNumb % 2 === 0) {
+        evenNUmbers.push(thisNumb)
+    }
+}
+console.log(evenNUmbers)
