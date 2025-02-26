e6b8984e6fc4cd9ad1e8d7023f6f33f2f3523e2c
NavodContrath <navod.contrath@gmail.com>
2025-02-26 15:09:09 +0100

    add map to third snack

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -2,3 +2,8 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+//map
+const newNumbers = numbers.map((numb) => {
+    return numb + 1
+})
+console.log(newNumbers)
