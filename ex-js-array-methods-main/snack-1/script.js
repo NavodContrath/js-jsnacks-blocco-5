85d30daf32b4d38d6caa4f9d8d2dfb53ccff44ea
NavodContrath <navod.contrath@gmail.com>
2025-02-26 15:06:22 +0100

    add for each loop in first  snack

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -2,6 +2,7 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+//for each loop
 names.forEach((name) => {
     console.log(name)
 })
